867221aadf94815b5d41ecf9af13ed9b579a38ad
divyansh <mah.divyansh@gmail.com>
2023-10-28 16:44:42 +0530

    login page

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Replace this with your actual authentication logic
+    if (username === "" && password === "") {
+      onLogin(true);
+    } else {
+      onLogin(false);
+    }
+  };
+
+  return (
+    <div className="login-page">
+    <div className="login-container">
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+    </div>
+  );
+};
+
+export default Login;
