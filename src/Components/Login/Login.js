33263c6eb1ca85a7772329e601fe7b74583994c4
divyansh <mah.divyansh@gmail.com>
2023-10-29 19:06:00 +0530

    added error validation functionality

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,40 +6,34 @@ const REACT_APP_ENDPOINT = process.env.REACT_APP_ENDPOINT;
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
+  const [error, setError] = useState(""); 
+  const handleErrorResponse = (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        return "Invalid email or password";
+      } else if (error.response.status === 404) {
+        return "User is not registered in the database";
+      }
+    }
+    return "An error occurred while logging in";
+  };
   const handleLogin = async (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
+    e.preventDefault();
+    setError(""); // Clear previous errors
 
     try {
-      const response = await axios.post(`${REACT_APP_ENDPOINT}/portfolio/login`, {
-        email,
-        password,
-      });
-      if (response.status === 200) {
-      console.log("Response status:", response.status);
-      console.log("Response data:", response.data);
-        onLogin(true);
-      } 
-      else if (response.status === 404) {
-        setError("User is not registered in the database");
-      } 
-      else if (response.status === 401) {
-        setError("Invalid email or password");
-      } 
-      else {
-        setError("An error occurred while logging in");
-        if (response.data && response.data.error) {
-          // Handle the specific error from the response data
-          setError(response.data.error);
+      const response = await axios.post(
+        `${REACT_APP_ENDPOINT}/portfolio/login`,
+        {
+          email,
+          password,
         }
-    
-      }
+      );
+      onLogin(true);
+      console.log("Login : " +  response.data)
     } catch (error) {
-      console.error("Login error:", error);
-      setError("An error occurred while logging in");
+      setError(handleErrorResponse(error));
     }
-    
   };
 
   return (
